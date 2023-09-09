6793bd1f6c9951636c9c296ee3a7029d5fafe177
darktend <Fredifox1935>
2023-09-09 11:21:39 +0300

    add HomePage

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,9 +1,9 @@
-async function Page(){
-    return(
-        <main>
-            <h1 className='head-text'>Onboarding</h1>
-        </main>
-    )
-};
+async function Page() {
+  return (
+    <main>
+      <h1 className="head-text">Onboarding</h1>
+    </main>
+  );
+}
 
 export default Page;
